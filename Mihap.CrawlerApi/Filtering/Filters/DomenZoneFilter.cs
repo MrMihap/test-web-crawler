b9a6f57c23637c629771402e0b80a474d8ea8fe8
Mikhail <mihail.tatarintsev@mikrokapital.com>
2020-05-07 16:07:44 +0300

    DomenZoneFilter

---
+++ Mihap.CrawlerApi/Filtering/Filters/DomenZoneFilter.cs
@@ -0,0 +1,41 @@
+﻿using Mihap.CrawlerApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mihap.CrawlerApi.Filtering
+{
+	public class DomenZoneFilter : BaseFilter
+	{
+		public string DomenZone { get; set; } = String.Empty;
+
+		public override bool Pass(Link link)
+		{
+			if (link == null) return false;
+			if (String.IsNullOrEmpty(DomenZone)) return false;
+
+
+
+			switch(filterMode)
+			{
+				case FilterMode.Pass:
+					if (GetDomenZone(link.Url).Contains(DomenZone))
+						return true;
+					else 
+						return false;
+
+				case FilterMode.NoPass:
+					if (!GetDomenZone(link.Url).Contains(DomenZone))
+						return true;
+					else
+						return false;
+			}
+			return false;
+		}
+		private string GetDomenZone(string url)
+		{
+			// MOCKED
+			return "com";
+		}
+	}
+}
